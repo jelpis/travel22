232944d55c2aabf59e57847d41be38af2fa1720c
sunny <jelpis27@gmail.com>
2023-02-27 15:59:29 +0900

    second finish

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './main.css';
-import img1 from '../Assets/img1.jpg'
-import img2 from '../Assets/img2.jpg'
-import img3 from '../Assets/img3.jpg'
-import img4 from '../Assets/img4.jpg'
-import img5 from '../Assets/img5.jpg'
-import img6 from '../Assets/img6.jpg'
-import img7 from '../Assets/img7.jpg'
-import img8 from '../Assets/img8.jpg'
-import img9 from '../Assets/img9.jpg'
+import img1 from '../Assets/img1.jpg';
+import img2 from '../Assets/img2.jpg';
+import img3 from '../Assets/img3.jpg';
+import img4 from '../Assets/img4.jpg';
+import img5 from '../Assets/img5.jpg';
+import img6 from '../Assets/img6.jpg';
+import img7 from '../Assets/img7.jpg';
+import img8 from '../Assets/img8.jpg';
+import img9 from '../Assets/img9.jpg';
 
-import {HiOutlineLocationMarker} from 'react-icons/hi'
-import {HiOutlineClipboardCheck} from 'react-icons/hi'
+import { HiOutlineLocationMarker } from 'react-icons/hi';
+import { HiOutlineClipboardCheck } from 'react-icons/hi';
+
+import Aos, { init } from 'aos';
+import 'aos/dist/aos.css';
 
 const Data = [
   {
@@ -110,36 +113,39 @@ const Data = [
 ];
 
 const Main = () => {
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
+
   return (
-    <section className='main container section'>
-
-       <div className="secTitle">
-         <h3 className="title">
+    <section className="main container section">
+      <div className="secTitle">
+        <h3 data-aos="fade-right" className="title">
           Most visited destinations
-         </h3>
-       </div>
+        </h3>
+      </div>
 
-        <div className="secContent grid">
-
-          {Data.map(({id, imgSrc, destTitle, location, grade, fees, description })=>{
+      <div className="secContent grid">
+        {Data.map(
+          ({ id, imgSrc, destTitle, location, grade, fees, description }) => {
             return (
-              <div key={id} className="singleDestination">
+              <div key={id} data-aos="fade-up" className="singleDestination">
                 <div className="imgaeDiv">
                   <img src={imgSrc} alt={destTitle} />
                 </div>
 
                 <div className="cardInfo">
-                  <h4 className="destTitle">
-                    {destTitle}
-                  </h4>
+                  <h4 className="destTitle">{destTitle}</h4>
                   <span className="continent flex">
-                    <HiOutlineLocationMarker className='icon'/>
+                    <HiOutlineLocationMarker className="icon" />
                     <span className="name">{location}</span>
                   </span>
 
                   <div className="fees flex">
                     <div className="grade">
-                      <span><small>+1</small></span>
+                      <span>
+                        <small>+1</small>
+                      </span>
                     </div>
                     <div className="price">
                       <h5>{fees}</h5>
@@ -152,18 +158,16 @@ const Main = () => {
 
                   <button className="btn flex">
                     DETAILS
-                    <HiOutlineClipboardCheck className='icon'/>
+                    <HiOutlineClipboardCheck className="icon" />
                   </button>
                 </div>
               </div>
-            )
-          })
-        }
-
-        </div>
-
+            );
+          }
+        )}
+      </div>
     </section>
-  )
+  );
 };
 
 export default Main;
